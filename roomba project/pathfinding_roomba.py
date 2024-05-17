b10096fee8b1375f63676ed747c3ffb2c927129a
hpjat237 <falai2372004@gmail.com>
2024-05-17 10:26:39 +0700

    add requirements.txt file

--- roomba project/pathfinding_roomba.py
+++ roomba project/pathfinding_roomba.py
@@ -1,4 +1,8 @@
 import pygame, sys 
+
+import streamlit as st
+import streamlit.components.v1 as components
+
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
